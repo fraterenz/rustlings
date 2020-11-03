c487de87ec6958e02a98902d8f7a4b59549fc666
Francesco Terenzi <fra.terenz1993@gmail.com>
2020-11-03 21:18:25 +0100

    checkpoint

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,17 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    //let a: [u16; 100] = [10; 10]; this is wrong since the expected
+    // size of a is 100 but got an array of 10 only elements
+    //
+    // this is wrong too since 
+    // let a: [std::string::String; 100] = ["10".to_string(); 100];
+    // the element is not type `Copy` since strings are stored on
+    // the heap: 'the trait `std::marker::Copy` is not implemented for 
+    // `std::string::String`'
+    let a: [u8; 100] = [10; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
