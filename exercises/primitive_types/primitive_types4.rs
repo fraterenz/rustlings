b664b80fcf3dc0097ac1ecfdae523b888c6d83d0
Francesco Terenzi <fra.terenz1993@gmail.com>
2020-11-05 21:28:35 +0100

    primitive

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,7 +2,6 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
@@ -10,5 +9,8 @@ fn slice_out_of_array() {
 
     let nice_slice = &a[1..4];
 
+    // this works because a slice implements the Deref trait
+    // and thus Defer coercion is used and we dont need to
+    // write &[2, 3, 4] or *nice_slice
     assert_eq!([2, 3, 4], nice_slice)
 }
